070e38b6c64dff447b0dca1ff4df424417f21abb
germa <gilberto.alvarez2016@uteq.edu.ec>
2020-07-23 18:31:01 -0500

    lectura de datos

--- app/src/main/java/com/example/pruebacorte1/holder/item.java
+++ app/src/main/java/com/example/pruebacorte1/holder/item.java
@@ -1,5 +1,38 @@
 package com.example.pruebacorte1.holder;
 
-public class item {
-}
+import android.content.Context;
+import android.widget.EditText;
 
+import com.example.pruebacorte1.R;
+import com.mindorks.placeholderview.Animation;
+import com.mindorks.placeholderview.PlaceHolderView;
+import com.mindorks.placeholderview.annotations.Animate;
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+
+
+@NonReusable
+@Layout(R.layout.gallery_item)
+public class item {
+    @View(R.id.txt1)
+    private EditText txt1;
+
+    @View(R.id.txt2)
+    private EditText txt2;
+
+    private String mUlr;
+    private Context mContext;
+    private PlaceHolderView mPlaceHolderView;
+
+
+    public item(Context context, PlaceHolderView placeHolderView, String url) {
+        mContext = context;
+        mPlaceHolderView = placeHolderView;
+        mUlr = url;
+    }
+
+
+
+}
