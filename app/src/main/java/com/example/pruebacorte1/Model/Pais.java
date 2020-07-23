826860a80554c6ac622398c1d2a694fb33c723f3
germa <gilberto.alvarez2016@uteq.edu.ec>
2020-07-23 17:40:29 -0500

    api de acceso a webservice2

--- app/src/main/java/com/example/pruebacorte1/Model/Pais.java
+++ app/src/main/java/com/example/pruebacorte1/Model/Pais.java
@@ -1,4 +1,27 @@
 package com.example.pruebacorte1.Model;
 
 public class Pais {
+    private String nombre;
+    private String codBand;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodBand() {
+        return codBand;
+    }
+
+    public void setCodBand(String codBand) {
+        this.codBand = codBand;
+    }
+
+    public Pais(String nombre, String codBand) {
+        this.nombre = nombre;
+        this.codBand = codBand;
+    }
 }
